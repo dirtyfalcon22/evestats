4137010c012568e02b0c19f70049a75370cf341a
Dirtyfalcon22 <dirtyfalcon22@gmail.com>
2018-06-14 10:03:46 -1000

    ...

---
+++ main.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        frame: false,
+        height: 700,
+        resizable: false,
+        width: 368
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+});
